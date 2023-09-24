0d9edb11e4543f2fca56907154a40398150c4854
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-09-24 19:16:34 +0200

    TESTED: apartment card

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,35 @@
 <template>
   <div class="body">
-    <Header>{{ title }}</Header>
-    <Button @click="increment" :outline="isVisible">Enter</Button>
-    <StarRating :rating="1.50"/>
+    <ApartmentsItem
+      :description="apartment.description"
+      :price="apartment.price"
+      :rating="apartment.rating"
+      :imgSrc="apartment.imgSrc"
+    />
   </div>
 </template>
 
 <script>
-import Button from './components/Button.vue';
-import Header from './components/Header.vue';
-import StarRating from './components/StarRating.vue';
-
+import ApartmentsItem from './components/apartments/index';
+import image from '../public/image/Rectangle 42.jpg';
 export default {
   name: 'Body',
   components: {
-    Header,
-    Button,
-    StarRating,
+    ApartmentsItem,
   },
   data() {
     return {
-      amountOfOrder: 0,
-      fruits: ['banana', 'kiwi'],
-      isVisible: false,
+      apartment: {
+        description:
+          'Роскошные апартаменты в скайндинавском стиле с панорамным видом на реку и бесплатным Wi-Fi.',
+        rating: 4,
+        price: 1500,
+        imgSrc: image,
+      },
     };
   },
-  computed: {
-    title() {
-      return `Yor total order is ${this.amountOfOrder}`;
-    },
-  },
-  methods: {
-    increment() {
-      return (this.amountOfOrder += 1), (this.isVisible = true);
-    },
-  },
+  computed: {},
+  methods: {},
 };
 </script>
 
@@ -43,7 +38,7 @@ body {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  font-family: Montserrat Medium;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
