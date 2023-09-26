b6c31d69e4983015662193cb683f7eeda52f5ea0
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-09-26 18:58:23 +0200

    ADDED: title

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="app">
-    <ApartmentsList :apartments="apartments" />
+    <ApartmentsList :items="apartments">
+      <template v-slot:title>Selection according to choice</template>
+      <template v-slot:apartment="{ apartment }">
+        <ApartmentsItem
+          :description="apartment.descr"
+          :price="apartment.price"
+          :rating="apartment.rating"
+          :imgSrc="apartment.imgUrl"
+          :key="apartment.id"
+        />
+      </template>
+    </ApartmentsList>
   </div>
 </template>
 
 <script>
 import apartments from './components/apartment/apartments';
+import ApartmentsItem from './components/apartment/ApartmentsItem.vue';
 import ApartmentsList from './components/apartment/ApartmentsList.vue';
 export default {
   name: 'App',
   components: {
     ApartmentsList,
+    ApartmentsItem,
   },
   data() {
     return {
