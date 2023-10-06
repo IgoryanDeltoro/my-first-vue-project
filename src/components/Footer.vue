c5e4a33fc21f7d224a65de49d3168b362efd7405
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-06 11:45:40 +0200

    CREATED: app footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/index.scss';
+.footer {
+  background-color: $bgc-header-footer;
+  height: 30px;
+  flex-shrink: 0;
+}
+</style>
