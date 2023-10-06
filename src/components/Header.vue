8ad1460402246ef5c5b0011d18bdb1e7602a53f8
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-06 11:45:53 +0200

    CREATED: app header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1><slot></slot></h1>
+  <header class="header">
+    <Container>
+      <Logo />
+    </Container>
+  </header>
 </template>
+
+<script>
+import Logo from './Logo.vue';
+import Container from './shared/Container.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    Logo,
+    Container,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/index.scss';
+.header {
+  padding: 16px 0;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background-color: $bgc-header-footer;
+}
+</style>
