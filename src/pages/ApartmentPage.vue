b5f5c236aeb10917e15cde6a1f2387e199920cb8
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-09 22:22:41 +0200

    ADDED: apartment page

---
+++ src/pages/ApartmentPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <Container>
+      <ApartmentMainInfo :apartment="apartment" />
+    </Container>
+  </main>
+</template>
+
+<script>
+import Container from '../components/shared/Container.vue';
+import ApartmentMainInfo from '../components/apartment/ApartmentMainInfo.vue';
+import apartments from '../components/apartment/apartments';
+
+export default {
+  name: 'ApartmentPage',
+  components: {
+    Container,
+    ApartmentMainInfo,
+  },
+  computed: {
+    apartment() {
+      return apartments.find(
+        apartment => apartment.id === this.$route.params.id
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
