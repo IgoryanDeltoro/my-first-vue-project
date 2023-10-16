549a08089a7156f760b517a2224a42e1a428e589
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-16 15:17:29 +0200

    ADDED: endpoints

---
+++ src/services/apartment.service.js
@@ -0,0 +1,13 @@
+import axios from '../utils/axios';
+
+export const getApartments = () => {
+  return axios.get('apartments');
+};
+
+export const getApartmentById = id => {
+  return axios.get(`apartments/${id}`);
+};
+
+export const getCities = () => {
+  return axios.get('cities');
+};
