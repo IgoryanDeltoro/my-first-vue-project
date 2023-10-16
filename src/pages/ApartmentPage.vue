07d8aa5a7779a5689abdce2f96de6ab9be5fe5de
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-16 11:49:31 +0200

    ADDED: components like reviews and owner

--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -1,7 +1,16 @@
 <template>
   <main>
     <Container>
-      <ApartmentMainInfo :apartment="apartment" />
+      <div class="apartment-page">
+        <ApartmentMainInfo :apartment="apartment" />
+        <div class="apartment-page__additional-info">
+          <ApartmentsOwner
+            class="apartment-page__owner"
+            :owner="apartment.owner"
+          />
+          <Reviews :reviews="reviews" />
+        </div>
+      </div>
     </Container>
   </main>
 </template>
@@ -10,14 +19,21 @@
 import Container from '../components/shared/Container.vue';
 import ApartmentMainInfo from '../components/apartment/ApartmentMainInfo.vue';
 import apartments from '../components/apartment/apartments';
-
+import ApartmentsOwner from '../components/apartment/ApartmentsOwner.vue';
+import Reviews from '../components/reviews/Reviews.vue';
+import reviews from '../components/reviews/reviews.json';
 export default {
   name: 'ApartmentPage',
   components: {
     Container,
     ApartmentMainInfo,
+    ApartmentsOwner,
+    Reviews,
   },
   computed: {
+    reviews() {
+      return reviews;
+    },
     apartment() {
       return apartments.find(
         apartment => apartment.id === this.$route.params.id
@@ -27,4 +43,20 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.apartment-page {
+  display: flex;
+  align-items: flex-start;
+
+  &__owner{
+    margin-bottom: 20px;
+  }
+
+  &__additional-info {
+    margin-left: 30px;
+    max-width: 350px;
+    flex-grow: 0;
+    flex-shrink: 1;
+  }
+}
+</style>
