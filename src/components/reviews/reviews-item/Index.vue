36d35fa13d1de756c964b7c611c2d857fea3433e
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-22 16:25:53 +0200

    RENAME: component name

---
+++ src/components/reviews/reviews-item/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="reviews-item">
+    <div class="reviews-item__content">
+      <ReviewsHeading :review="review" />
+    </div>
+    <p class="reviews-item__descr">{{ review.content }}</p>
+  </section>
+</template>
+
+<script>
+import ReviewsHeading from './ReviewsHeading.vue';
+
+export default {
+  name: 'ReviewsItem',
+  props: {
+    review: {
+      types: Object,
+      required: true,
+    },
+  },
+  components: {
+    ReviewsHeading,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/scss/index.scss';
+
+.reviews-item {
+  padding: 20px;
+  position: relative;
+
+  border-top: 1px solid $second-bgc;
+  border-left: 2px solid $second-bgc;
+  border-right: 2px solid $second-bgc;
+  border-bottom: 1px solid $second-bgc;
+
+  &__descr {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.2;
+  }
+}
+</style>
