d10ac1ebd098bbbc336f8e2e170d86b69b3f51ae
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-24 21:55:58 +0200

    ADDED:  login rules validation

--- src/utils/validationRules.js
+++ src/utils/validationRules.js
@@ -3,7 +3,17 @@ export const isRequired = value => ({
   message: 'The field must not empty!',
 });
 
-export const charLimit = limit => value => ({  
+export const charLimit = limit => value => ({
   hasPassed: value.toString().length <= limit,
   message: 'Chars are too many then must be!',
+});
+
+export const emailValidation = val => ({
+  hasPassed: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(val),
+  message: 'Email is wrong',
+});
+
+export const passwordValidation = val => ({
+  hasPassed: /^(?=.*[A-Za-z])(?=.*[\d])[a-zA-Z0-9]{7,256}$/.test(val),
+  message: 'The password must contain one number',
 });
