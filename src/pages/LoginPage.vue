58c9a4a50215afce5f0e8befce508f7ade30bfb1
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-24 21:58:57 +0200

    ADDED: login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="login">
+    <Login />
+  </main>
+</template>
+
+<script>
+import Login from '../components/login/index.vue';
+export default {
+  name: 'LoginPage',
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
