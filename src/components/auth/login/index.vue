bcb2a3d522423306d504374bb1c550c69213bea6
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-26 13:23:40 +0200

    CREATED: login component

---
+++ src/components/auth/login/index.vue
@@ -0,0 +1,117 @@
+<template>
+  <AuthContainer class="login">
+    <MainTitle class="login__title">Login</MainTitle>
+    <Form ref="form" @:submit.prevent="handleSubmit">
+      <CustomInput
+        :rules="emailRules"
+        v-model="formData.email"
+        name="email"
+        type="email"
+        class="form__input form__input--first"
+        :success="success"
+      />
+      <CustomInput
+        :rules="passwordRules"
+        v-model="formData.password"
+        name="password"
+        type="password"
+        class="form__input form__input--last"
+        :success="success"
+       
+      />
+      <Button class="form__button" type="submit">Enter</Button>
+    </Form>
+  </AuthContainer>
+</template>
+
+<script>
+import Form from '../../shared/form/index.vue';
+import CustomInput from '../../shared/CustomInput.vue';
+import Button from '../../Button.vue';
+import AuthContainer from '../../auth/AuthContainer.vue';
+import {
+  emailValidation,
+  passwordValidation,
+  isRequired,
+} from '../../../utils/validationRules';
+import MainTitle from '../../shared/MainTitle.vue';
+import { login } from '../../../services/apartment.service';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: '',
+      },
+      success: false,
+    };
+  },
+  components: {
+    Form,
+    CustomInput,
+    Button,
+    AuthContainer,
+    MainTitle,
+  },
+  computed: {
+    rules() {
+      return { emailValidation, passwordValidation, isRequired };
+    },
+
+    emailRules() {
+      return [this.rules.isRequired, this.rules.emailValidation];
+    },
+    passwordRules() {
+      return [this.rules.passwordValidation];
+    },
+  },
+  methods: {
+    async handleSubmit() {
+      const isFormValid = this.$refs.form.validate();
+      this.$refs.form.reset();
+
+      if (isFormValid) {
+        this.success = true;
+
+        try {
+          const { data } = await login(this.formData);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &__title {
+    margin-bottom: 18px;
+  }
+}
+.form {
+  &__input {
+    width: 100%;
+
+    &--first {
+      margin-bottom: 23px;
+    }
+
+    &--last {
+      margin-bottom: 37px;
+    }
+  }
+
+  &__button {
+    width: 100%;
+  }
+}
+</style>
