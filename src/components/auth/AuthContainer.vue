e123ed9106507eaa6bb8eec13a3d80ec7bb8f808
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-26 13:23:08 +0200

    CREATED: custom auth container

---
+++ src/components/auth/AuthContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="auth-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthContainer',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables.scss';
+
+.auth-container {
+  padding: 30px;
+  max-width: 410px;
+  width: 100%;
+  background-color: $white-color;
+}
+</style>
