af8cc7e3e5adb698da180def3f32b6d2024983ef
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-26 13:24:40 +0200

    CREATED: custom main title

---
+++ src/components/shared/MainTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <component class="general-title" :is="tagName">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'MainTitle',
+  props: {
+    level: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    tagName() {
+      return `h${this.level}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.general-title {
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 1.4;
+}
+</style>
