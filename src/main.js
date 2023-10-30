af08274c49bde8f4c912040ba50dc76073db5b96
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-10-30 13:52:01 +0100

    INITIALED: store

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue';
 import router from './router';
 import Notifications from '@kyvg/vue3-notification';
 import './assets/scss/index.scss';
+import store from './components/store';
+
 const app = createApp(App);
 
 app.use(router);
+app.use(store)
 app.use(Notifications);
 app.mount('#app');
