d492515d01eb9ea6cd252cba60d2e56df0057186
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-11-03 13:30:50 +0100

    RENAMED: name

---
+++ src/components/header/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <Container>
+      <div class="header__content">
+        <router-link to="/">
+          <Logo />
+        </router-link>
+        <AuthActions />
+      </div>
+    </Container>
+  </header>
+</template>
+
+<script>
+import Logo from '../Logo.vue';
+import Container from '../shared/Container.vue';
+import AuthActions from './AuthActions.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    Logo,
+    Container,
+    AuthActions,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/index.scss';
+.header {
+  padding: 16px 0;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background-color: $bgc-header-footer;
+
+  &__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
