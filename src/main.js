42c97c6a7cc94aa5a0733f668c1e91e1eed72015
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-11-06 11:54:22 +0100

    RENAMED: import path

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import Notifications from '@kyvg/vue3-notification';
 import './assets/scss/index.scss';
-import store from './components/store';
+import store from './store/index';
 
 const app = createApp(App);
 store.dispatch('auth/loadDataFromLS');
