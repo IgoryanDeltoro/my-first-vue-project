dabf4f3f0983129d94d5e4003d5290f3b2ed41c6
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-11-07 19:30:43 +0100

    CREATED: orders list

---
+++ src/components/my-orders/OrdersList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="orders-list">
+    <OrdersItem
+      v-for="order in orders"
+      :key="order.id"
+      :order="order.apartment"
+    />
+  </div>
+</template>
+
+<script>
+import OrdersItem from './OrdersItem.vue';
+
+export default {
+  name: 'OrdersList',
+  components: {
+    OrdersItem,
+  },
+  props: {
+    orders: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
