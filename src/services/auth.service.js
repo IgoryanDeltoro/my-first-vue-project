b78e004d207a5ec0ba6beab7ce87ec1831c105c7
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:02:30 +0100

    ADDED: opportunity to update user profile

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,17 @@
 import axios from '../utils/axios';
 
-export const login = payload => {
-    return axios.post('users/login', payload);
-  };
-  
-  export const register = payload => {
-    return axios.post('users/register', payload);
-  };
-  
-  export const logout = () => {
-    return axios.post('users/logout');
-  };
+export const login = data => {
+  return axios.post('users/login', data);
+};
+
+export const register = data => {
+  return axios.post('users/register', data);
+};
+
+export const logout = () => {
+  return axios.post('users/logout');
+};
+
+export const updateUserProfile = data => {
+  return axios.patch('users/avatars', data);
+};
