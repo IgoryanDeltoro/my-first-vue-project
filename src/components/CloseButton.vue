9516e9ab6803439d5aca8e94c18858e7c9de782b
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:09:46 +0100

    CREATED: the custom close button

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="close-button" @click="onCloseButton">
+    <CloseIcon class="close-button__icon" />
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import CloseIcon from '../assets/svg/close.svg';
+export default {
+  name: 'CloseButton',
+  components: {
+    CloseIcon,
+  },
+  methods: {
+    onCloseButton() {
+      this.$emit('closeModal', false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '.././assets/scss/variables.scss';
+
+.close-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 50%;
+  background-color: $white-color;
+
+  &:hover {
+    background-color: $gray-border-color;
+  }
+
+  &__icon {
+    width: 18px;
+    height: 18px;
+  }
+}
+</style>
