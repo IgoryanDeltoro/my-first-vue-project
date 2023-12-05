b2ea1e2b2751f3c4c5c49b1dbc7be349241545ee
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 17:57:52 +0100

    ADDED: reviews modal window

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
       <notifications />
       <Header />
       <router-view></router-view>
+      <ReviewsModal
+        v-if="isShowModal"
+        :rating="rating"
+      />
     </div>
     <Footer />
   </div>
@@ -12,12 +16,31 @@
 <script>
 import Footer from './components/Footer.vue';
 import Header from './components/header/index.vue';
+import ReviewsModal from './components/reviews/modal/ReviewsModal.vue';
 
 export default {
   name: 'App',
   components: {
     Footer,
     Header,
+    ReviewsModal,
+  },
+  data() {
+    return {
+      isShowModal: false,
+      rating: 0,
+    };
+  },
+  provide() {
+    return {
+      div: this,
+    };
+  },
+  methods: {
+    showModal(value, rating) {
+      this.isShowModal = value;
+      this.rating = rating;
+    },
   },
 };
 </script>
