29458d60cc60bae8edf293dbcd12c1fe1b5c63cc
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:10:50 +0100

    ADDED: conditions for  changing color

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,10 +1,29 @@
 <template>
-  <img src="../assets/svg/logo.svg" alt="logo" />
+  <router-link to="/">
+    <img v-if="switch" src="../assets/svg/logo.svg" alt="logo" />
+    <img v-else src="../assets/svg/logo-2.svg" alt="logo" />
+  </router-link>
 </template>
 
 <script>
 export default {
   name: 'Logo',
+  data() {
+    return {
+      switch: true,
+    };
+  },
+  props: {
+    color: {
+      type: String,
+      default: '#fff',
+    },
+  },
+
+  created() {
+    const color = ['black', '#000000', '#000'];
+    this.switch = color.includes(this.color) ? false : true;
+  },
 };
 </script>
 
