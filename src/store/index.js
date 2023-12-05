c90eb04153e88bb467d957fc77e280ccd6438a86
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:03:07 +0100

    ADDED: booking module

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vuex from 'vuex';
 import authModules from './modules/auth';
+import bookingModules from './modules/booking';
+
 
 const store = new Vuex.Store({
   modules: {
     auth: authModules,
+    booking: bookingModules,
   },
 });
 
