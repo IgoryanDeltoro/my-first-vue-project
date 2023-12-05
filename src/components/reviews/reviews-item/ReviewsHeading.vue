7b7d91e02704e31ff2c7e69cd82bb53ce2ad5908
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:30:32 +0100

    ADDED: custom user avatar

--- src/components/reviews/reviews-item/ReviewsHeading.vue
+++ src/components/reviews/reviews-item/ReviewsHeading.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="reviews-heading">
-    <ReviewsAvatar />
+    <UserAvatar class="reviews-heading__photo" :src="review.avatar" />
     <div class="reviews-heading__content">
       <span class="reviews-heading__author">{{ review.author }}</span>
-      <StarRating :rating="review.rating" />
+      <StarRating :rating="review.rating" :switch="false" />
     </div>
   </div>
 </template>
 <script>
 import StarRating from '../../StarRating.vue';
-import ReviewsAvatar from './ReviewsAvatar.vue';
+import UserAvatar from '../../shared/UserAvatar.vue';
 
 export default {
   name: 'ReviewHeading',
   components: {
-    ReviewsAvatar,
     StarRating,
+    UserAvatar,
   },
   props: {
     review: {
@@ -34,9 +34,16 @@ export default {
   &__content {
     margin-left: 20px;
   }
+  &__photo {
+    width: 34px;
+    height: 34px;
+  }
   &__author {
     display: block;
     margin-bottom: 2px;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 1.2;
   }
 }
 </style>
