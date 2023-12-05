fec4947703aa3f949addf310ecc159d7cc600ff0
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:31:12 +0100

    ADDED: custom user component

---
+++ src/components/shared/UserAvatar.vue
@@ -0,0 +1,16 @@
+<template>
+  <img class="user-avatar"  alt="user photo" />
+</template>
+
+<script>
+
+export default {
+  name: 'UserAvatar',
+};
+</script>
+
+<style lang="scss" scoped>
+.user-avatar {
+  border-radius: 50%;
+}
+</style>
