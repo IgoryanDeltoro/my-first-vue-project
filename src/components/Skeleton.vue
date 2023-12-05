adc78be2c493653e7d141fee1545a925e6b11579
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-05 18:11:16 +0100

    CREATED: custom skeleton

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="skeleton">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Skeleton',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '.././assets/scss/variables.scss';
+.skeleton {
+  animation: pulse-bg 1000ms linear infinite ;
+}
+@keyframes pulse-bg {
+  0% {
+    background-color: #ddd;
+  }
+  50% {
+    background-color: #d0d0d0;
+  }
+  100% {
+    background-color: #ddd;
+  }
+}
+</style>
