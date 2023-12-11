7927241a3bdc4c49662c46c4a1cd4b89cb9b1d9b
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-11 19:52:22 +0100

    INITIALED: vue pagination

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import Notifications from '@kyvg/vue3-notification';
-import './assets/scss/index.scss';
 import store from './store/index';
+import Notifications from '@kyvg/vue3-notification';
+import VueAwesomePaginate from 'vue-awesome-paginate';
+import './assets/scss/index.scss';
+import 'vue-awesome-paginate/dist/style.css';
 
 const app = createApp(App);
-store.dispatch('auth/loadDataFromLS');
 
 app.use(router);
 app.use(store);
 app.use(Notifications);
+app.use(VueAwesomePaginate);
+
+store.dispatch('auth/loadDataFromLS');
+store.dispatch('booking/getApartmentsList');
+
 app.mount('#app');
