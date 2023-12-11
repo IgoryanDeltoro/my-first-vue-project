3f1e7770fa47e102e4a09d373d38215aaa46ad5b
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-12-11 19:52:52 +0100

    CREATED: pagination vue component

---
+++ src/components/apartment/Pagination.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="pagination-box">
+    <vue-awesome-paginate
+      :total-items="20"
+      :items-per-page="4"
+      :max-pages-shown="5"
+      v-model="currentPage"
+      paginate-buttons-class="btn"
+      active-page-class="btn-active"
+      back-button-class="back-btn"
+      next-button-class="next-btn"
+      :on-click="onClickHandler"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+import { ref } from 'vue';
+
+export default {
+  name: 'Pagination',
+  data() {
+    return {
+      currentPage: ref(1),
+    };
+  },
+  methods: {
+    ...mapMutations('booking', ['SET_CURRENT_PAGE']),
+
+    async onClickHandler() {
+      this.SET_CURRENT_PAGE(this.currentPage);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../assets/scss/variables.scss';
+
+ul#componentContainer.pagination-container {
+}
+.btn {
+  height: 40px;
+  width: 40px;
+  border: none;
+  margin-inline: 5px;
+  cursor: pointer;
+}
+.back-btn {
+  background-color: $second-bgc;
+}
+.next-btn {
+  background-color: $second-bgc;
+}
+.btn-active {
+  background-color: $main-color;
+  color: $white-color;
+}
+</style>
